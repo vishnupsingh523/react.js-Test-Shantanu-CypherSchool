8c63b5101d9fac8ee09dd133ca574a8055e2da90
Vishwanath Pratap Singh <vishnupsingh523@gmail.com>
2022-06-14 09:23:43 +0530

    all files added

---
+++ src/App.js
@@ -0,0 +1,17 @@
+// import logo from './logo.svg';
+import './App.css';
+
+
+function App() {
+  return (
+    <div className="App">
+      <img src="images/vishnu.jpg" />
+      <h1 className='hii'>Hii there!</h1>
+
+      <h2>This is <snap>Vishwanath Pratap Singh</snap></h2>
+      <p>I am from a holy place Chitrakoot, Uttar Pradesh. I am persuing Bachelor's of Technology in Computer Science and Engineering from Lovely Professional University. I have experties in some programming languages like C, C++, Java and Python. I am a front-end webdeveloper and build some projects using some languages like: HTML, CSS, JavaScript.</p>
+    </div>
+  );
+}
+
+export default App;
